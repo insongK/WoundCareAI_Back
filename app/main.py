eef54553a28263346093f61d972e32fe0f4b43aa
insonggim <insonggim8@gmail.com>
2025-04-27 04:41:55 +0900

    "/" 라우트 추가

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ from app.api.v1 import api_router
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api/v1")
+
+@app.get("/")
+async def root():
+    return {"message": "WoundCareAI API is running."}
